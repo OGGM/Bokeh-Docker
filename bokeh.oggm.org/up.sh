bc8a683eaf57b55c9784ad4200b011a2ce45a913
Timo Rothenpieler <timo.rothenpieler@uni-bremen.de>
2019-11-06 19:26:12 +0100

    Update for static files

--- bokeh.oggm.org/up.sh
+++ bokeh.oggm.org/up.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+
+docker-compose() {
+	docker run --rm \
+		-v /var/run/docker.sock:/var/run/docker.sock \
+		-v "$PWD:$PWD" \
+		-w="$PWD" \
+		-e WS_ORIGIN \
+		docker/compose:1.24.1 "$@"
+}
+
 cd "$(dirname "$0")"
-docker run --rm \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    -v "$PWD:$PWD" \
-    -w="$PWD" \
-    -e WS_ORIGIN \
-    docker/compose:1.24.1 up "$@"
+docker-compose down
+docker-compose build --pull
+docker-compose up "$@"
