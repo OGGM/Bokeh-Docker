349dded6ac3946134d1a3a2cf0c9e9337e40c60e
Timo Rothenpieler <timo.rothenpieler@uni-bremen.de>
2019-11-07 19:00:28 +0100

    Add runner image

---
+++ bokeh.oggm.org/runner/run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -xe
+
+git clone https://github.com/OGGM/Bokeh-Docker.git repo
+cd repo/bokeh.oggm.org
+
+docker-compose down --remove-orphans
+docker system prune -f
+docker-compose build --pull
+docker-compose up -d
+
+while true; do
+	sleep 300
+	git fetch || continue
+	if [ $(git rev-parse @) != $(git rev-parse FETCH_HEAD) ]; then
+		git reset --hard FETCH_HEAD
+		git clean -fxd
+
+		docker-compose down --remove-orphans
+		docker system prune -f
+		docker-compose build --pull
+		docker-compose up -d
+	fi
+done
