94ee5d52639728d8f7128ae1751aef08c158e746
Timo Rothenpieler <timo.rothenpieler@uni-bremen.de>
2019-11-11 14:38:57 +0100

    Fix path to runner in up script

--- bokeh.oggm.org/up.sh
+++ bokeh.oggm.org/up.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 cd "$(dirname "$0")"
-docker build --pull -t oggm-bokeh-runner runner
+docker build --pull -t oggm-bokeh-runner ../runner
 docker rm --force oggm-bokeh-runner || true
 docker run -d -v /var/run/docker.sock:/var/run/docker.sock --restart unless-stopped --name oggm-bokeh-runner oggm-bokeh-runner
